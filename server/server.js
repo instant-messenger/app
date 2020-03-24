6dcaef02a4ea47a8cbaa99791e07825c9e2cdab4
angel-torres <alas4224@gmail.com>
2020-03-24 13:41:02 -0500

    Co-authored-by: Tavera15 <joaquinramirez97@yahoo.com

--- server/server.js
+++ server/server.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const server = express();
 
+server.get('/', (request, response) => {
+    response.send('<h1>Welcome to Instant Messenger</h1>');
+});
+
 module.exports = server;
