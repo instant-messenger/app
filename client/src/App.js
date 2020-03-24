e1f717d4c96053efd9f119480e17f7ac9cd6d60f
angel-torres <alas4224@gmail.com>
2020-03-24 14:23:46 -0500

    was able to get socket io client and server to talk to each other

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import socketIOClient from "socket.io-client";
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    console.log("logging from App");
+    const socket = socketIOClient("http://localhost:4000");
+    socket.on("connection", data => this.setState({ response: data }));
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
