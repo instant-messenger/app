f73896f312d2a46f0019e2ff745576cf5dc50a5d
angel-torres <alas4224@gmail.com>
2020-03-24 14:12:38 -0500

    removed yarn lock and added package-lock.json
    Co-authored-by: Tavera15 <joaquinramirez97@yahoo.com

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-const server = express();
-
-server.get('/', (request, response) => {
+app.get('/', (request, response) => {
     response.send('<h1>Welcome to Instant Messenger</h1>');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
 });
 
 module.exports = server;
