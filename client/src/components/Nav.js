eeca112647b4f94f13cc3f38d1260b78e2fe8354
angel-torres <alas4224@gmail.com>
2020-03-26 20:33:20 -0500

    fixed alignment in navigation

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+// STYLES
+import './styles/nav.css';
+
 export default function Nav() {
     return (
         <div className="nav-container">
-            <h1>Navigation</h1> 
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/dashboard">Dashboard</NavLink>
+            <h1>Instant Messenger</h1> 
+            <div className="navlink-container">
+                <NavLink to="/login">Login</NavLink>
+                <NavLink to="/dashboard">Dashboard</NavLink>
+            </div>
         </div>
     )
 }
