39db1e9336a611dc8011c2a971259ec06b4f7ab7
angel-torres <alas4224@gmail.com>
2020-04-11 14:57:24 -0500

    removed links login and register from nav; added link to title that takes you to register veiw

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 // STYLES
 import './styles/nav.scss';
@@ -7,10 +7,8 @@ import './styles/nav.scss';
 export default function Nav() {
     return (
         <div className="nav-container">
-            <h1>Instant Messenger</h1> 
+            <Link to="/"><h1>Instant Messenger</h1> </Link>
             <div className="navlink-container">
-                <NavLink to="/register">Register</NavLink>
-                <NavLink to="/login">Login</NavLink>
                 <NavLink to="/dashboard">Dashboard</NavLink>
             </div>
         </div>
